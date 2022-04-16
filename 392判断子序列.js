dee1013d5e479fc9a57366bdb7a3331b489dda1d
young-peace <yang97575129@163.com>
2022-04-16 23:36:43 +0800

    392

---
+++ 392判断子序列.js
@@ -0,0 +1,11 @@
+var isSubsequence = function (s, t) {
+    let i=0,j=0;
+    while(i<s.length && j<t.length){
+        if (s[i] == t[j]) { 
+            i++;
+        } 
+        j++;
+    }
+    return i==s.length;
+};
+console.log(isSubsequence("acb","ahbgdc"))
